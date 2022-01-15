86d6872add39bd32c2ebe611c2d445fa23ceb792
David Flindall <david@curiousways.com>
2022-01-15 19:52:45 +0000

    first draft
    Signed-off-by: David Flindall <david@curiousways.com>

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
